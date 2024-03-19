453ea8925c4b851f9243a401904712e92da09e7c
ggmsbsb <guilhermem20g@gmail.com>
2024-03-19 19:53:25 -0300

    Ex6 finalizado

--- lista/ex6.py
+++ lista/ex6.py
@@ -2,15 +2,32 @@ import pandas as pd
 import json
 import os
 
-# Construa o caminho completo para o arquivo JSON
+#Caminho JSON (tem q mudar dps)
 caminho_arquivo = os.path.join(os.getcwd(), 'lista', 'ex6.json')
-
-# Carrega os dados do arquivo JSON
+#Carregar json
 with open(caminho_arquivo, 'r') as f:
     data = json.load(f)
-
-# Acessa a lista de legendasfabricantes
 legendasfabricantes = data['legendasfabricantes']
+#print(legendasfabricantes) Só pra testar, depois remover
 
-# Agora você pode usar a lista como desejar
-print(legendasfabricantes)
+
+df = pd.DataFrame(legendasfabricantes, columns=['Legenda'])
+#Qtd de cada legenda
+qtd = df['Legenda'].value_counts().sort_index()
+#Per 100tual
+pcem = qtd / len(df) * 100
+
+#Solicitação da legenda desejada
+legenda_desejada = input("Legenda para consulta: ")
+
+if legenda_desejada in qtd.index:
+    qtd_legenda = qtd[legenda_desejada]
+    pct_legenda = pcem[legenda_desejada]
+
+#resultados 
+    print(f"A legenda especificada possui as seguintes características:") 
+    print(f"Legenda solicitada: {legenda_desejada}") 
+    print(f"Quantidade de fabricantes com essa legenda: {qtd_legenda}") 
+    print(f"Percentual em relação ao total: {pct_legenda:.2f}%") 
+else: 
+    print("Legenda não encontrada no DataFrame.")
