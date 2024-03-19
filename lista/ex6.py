aced52b3aa206e17ff29eb9c8c73ef60a235767b
ggmsbsb <guilhermem20g@gmail.com>
2024-03-19 19:37:24 -0300

    Json ex6

--- lista/ex6.py
+++ lista/ex6.py
@@ -1,1 +1,16 @@
 import pandas as pd
+import json
+import os
+
+# Construa o caminho completo para o arquivo JSON
+caminho_arquivo = os.path.join(os.getcwd(), 'lista', 'ex6.json')
+
+# Carrega os dados do arquivo JSON
+with open(caminho_arquivo, 'r') as f:
+    data = json.load(f)
+
+# Acessa a lista de legendasfabricantes
+legendasfabricantes = data['legendasfabricantes']
+
+# Agora você pode usar a lista como desejar
+print(legendasfabricantes)
